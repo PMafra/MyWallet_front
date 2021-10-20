5a6e06bb6aad070b07fb898c616ada3b7fc96bce
PMafra <pmafrarm@gmail.com>
2021-10-20 16:45:20 -0300

    Records component layout finished; container now scrolling

--- src/components/Records.js
+++ src/components/Records.js
@@ -11,9 +11,17 @@ export default function Records () {
                 </StyledNoRecords>
             ) : ("")}
             <StyledRecordsList>
-                <StyledRecord></StyledRecord>
+                <StyledRecord>
+                    <time className="fontClass">30/11</time>
+                    <p className="fontClass">Mercado alalla lasdasddd asdfsadfsda asdf churrasc paparoor</p>
+                    <span className="fontClass">50000000,00</span>
+                </StyledRecord>
             </StyledRecordsList>
-            <StyledBalanceBox></StyledBalanceBox>
+
+            <StyledBalanceBox>
+                <h3>SALDO</h3>
+                <span>VALUE</span>
+            </StyledBalanceBox>
         </StyledRecordsContainer>
     )
 }
@@ -26,7 +34,8 @@ const StyledRecordsContainer = styled.div`
     height: 100vh;
     background-color: #ffffff;
     border-radius: 5px;
-    padding: 23px 12px 10px 15px;
+    padding: 0 12px 0 15px;
+    position: relative;
 `
 const StyledNoRecords = styled.div`
     width: 100%;
@@ -47,14 +56,68 @@ const StyledNoRecords = styled.div`
 const StyledRecordsList = styled.ul`
     display: flex;
     flex-direction: column;
-    gap: 23px;
+    gap: 20px;
+    height: calc(100vh - 263px);
+    overflow-y: scroll;
+    padding: 15px 0px;
+
+    ::-webkit-scrollbar {
+        display: none;
+    }
 `
 const StyledRecord = styled.li`
     display: flex;
     flex-direction: row;
+
+    .fontClass {
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 19px;
+    }
+
+    time {
+        color: #D4D4D4;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-right: 10px;
+    }
+
+    p {
+        color: #000000;
+        word-break: break-all;
+    }
+
+    span {
+        color: #69CB60;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-left: 10px;
+    }
+
 `
 const StyledBalanceBox = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
+    width: calc(100% - 27px);
+    position: absolute;
+    bottom: 10px;
+    border-top: 1px solid #e8e8e8;
+    padding-top: 8px;
+    box-shadow: 0 -6px 5px -5px #d8d8d8;
+    
+    h3 {
+        font-weight: 700;
+        font-size: 17px;
+        line-height: 20px;
+        color: #000000;
+    }
+    span {
+        font-weight: 400;
+        font-size: 17px;
+        line-height: 20px;
+        color: #03AC00;
+    }
 `
